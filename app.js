cc6b5bcc4233242f17f057a08fe36d1267a1e73a
ilanlavi <refaelberlin@gmail.com>
2022-03-24 23:53:13 +0100

    initial setup

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile('./views/index.html', { root: __dirname });
 });
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname });
+});
+app.get('/works', (req, res) => {
+    res.sendFile('./views/works.html', { root: __dirname });
+});
+app.get('/gallery', (req, res) => {
+    res.sendFile('./views/gallery.html', { root: __dirname });
+});
 app.use((req, res) => {
     res.status(404).sendFile('./views/404.html', { root: __dirname });
 });
